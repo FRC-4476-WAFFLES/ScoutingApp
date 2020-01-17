9ff365f93912bb78c6aba1e09992d541c264fe7c
duncanstevenson <dmiester1965@gmail.com>
2020-01-17 18:38:28 -0500

    camera works!
    thanks ben

--- app/src/main/java/com/wcr/wafflesscoutingapp/GameId2/ScoutPicActivity.java
+++ app/src/main/java/com/wcr/wafflesscoutingapp/GameId2/ScoutPicActivity.java
@@ -44,9 +44,9 @@ public class ScoutPicActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(app_data.isWriteExternalStorageGranted()){
-//                    openCameraIntent();
+                    openCameraIntent();
 //                    openCamera();
-                    openBackCamera();
+//                    openBackCamera();
                 }
             }
         });
@@ -67,43 +67,36 @@ public class ScoutPicActivity extends AppCompatActivity {
         startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == 1001){
-            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
-            File Root = Environment.getExternalStorageDirectory();
-            File Dir = new File(Root.getAbsoluteFile() + "/WafflesScoutingAppData");
-
-            if (!Dir.exists()) {
-                Dir.mkdir();
-            }
-            File file = new File(Dir, "4476.jpg");
-            try (FileOutputStream out = new FileOutputStream(file)) {
-                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out); // bmp is your Bitmap instance
-                Log.d(TAG, "saved??");
-                // PNG is a lossless format, the compression factor (100) is ignored
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        if(requestCode == 1001){
+//            Bundle extras = data.getExtras();
+////            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+//            File Root = Environment.getExternalStorageDirectory();
+//            File Dir = new File(Root.getAbsoluteFile() + "/WafflesScoutingAppData");
+//
+//            if (!Dir.exists()) {
+//                Dir.mkdir();
+//            }
+//            File file = new File(Dir, "4476.jpg");
+//            try (FileOutputStream out = new FileOutputStream(file)) {
+//                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out); // bmp is your Bitmap instance
+//                Log.d(TAG, "saved??");
+//                // PNG is a lossless format, the compression factor (100) is ignored
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     String imageFilePath;
     private File createImageFile() throws IOException {
-        String timeStamp =
-                new SimpleDateFormat("yyyyMMdd_HHmmss",
-                        Locale.getDefault()).format(new Date());
-        String imageFileName = "IMG_" + timeStamp + "_";
+        String imageFileName = "4476";
         File storageDir =
                 getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
+        File image = new File(storageDir, imageFileName+".jpg");
+        image.createNewFile();
         imageFilePath = image.getAbsolutePath();
         return image;
     }
@@ -122,26 +115,13 @@ public class ScoutPicActivity extends AppCompatActivity {
             }
             if (photoFile != null) {
 //                Uri photoURI = FileProvider.getUriForFile(this, getApplicationContext().getPackageName(), photoFile);
-                Uri photoURI = Uri.fromFile(photoFile);
+                Uri photoURI = FileProvider.getUriForFile(this, "com.wcr.fileprovider", photoFile);
+//                Uri photoURI = Uri.fromFile(photoFile);
                 pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
                         photoURI);
                 startActivityForResult(pictureIntent,
                         REQUEST_CAPTURE_IMAGE);
             }
         }
-    }
-
-    private String pictureImagePath = "";
-    private void openBackCamera() {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "4476" + ".jpg";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        pictureImagePath = storageDir.getAbsolutePath() + "/" + imageFileName;
-        File file = new File(pictureImagePath);
-        Uri outputFileUri = Uri.fromFile(file);
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-        startActivityForResult(cameraIntent, 1);
     }
 }
