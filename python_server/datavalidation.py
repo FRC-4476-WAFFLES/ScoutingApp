7b514837f2f44fc0fcd8256c3f2bb664a4bf86a4
Brennan Bibic <brennan.bibic@gmail.com>
2020-02-27 17:14:49 -0500

    Data validation 1.0 should be good now

--- python_server/datavalidation.py
+++ python_server/datavalidation.py
@@ -61,6 +61,22 @@ def findErrors(df):
     print(blueScore)
     print(redScore)
     scoreBreakdown = match_data["score_breakdown"]
+
+    # Set counters
+    redInnerAuto = 0
+    redOuterAuto = 0
+    redLowerAuto = 0
+    blueInnerAuto = 0
+    blueOuterAuto = 0
+    blueLowerAuto = 0
+
+    redInnerTele = 0
+    redOuterTele = 0
+    redLowerTele = 0
+    blueInnerTele = 0
+    blueOuterTele = 0
+    blueLowerTele = 0
+
     for index, row in df.iterrows():
         # Get alliance station
         alliance = row['Alliance'][:1]
@@ -68,8 +84,27 @@ def findErrors(df):
         # Get correct alliance data
         if alliance == "R":
             allianceBreakdown = scoreBreakdown['red']
+
+            # AUTO SCORING
+            redInnerAuto += row["A_Inner"]
+            redOuterAuto += row['A_Outer']
+            redLowerAuto += row["A_Lower"]
+
+            # TELE SCORING
+            redInnerTele += row["T_Inner"]
+            redOuterTele += row['T_Outer']
+            redLowerTele += row["T_Lower"]
         else:
             allianceBreakdown = scoreBreakdown['blue']
+            # AUTO SCORING
+            blueInnerAuto += row["A_Inner"]
+            blueOuterAuto += row['A_Outer']
+            blueLowerAuto += row["A_Lower"]
+
+            # TELE SCORING
+            blueInnerTele += row["T_Inner"]
+            blueOuterTele += row['T_Outer']
+            blueLowerTele += row["T_Lower"]
         # Gets known values on a per robot basis
         measuredMovement = allianceBreakdown[f'initLineRobot{drive_station}']
 
@@ -81,14 +116,89 @@ def findErrors(df):
             measuredMovement = 0
         # Check if value of initiation line is correct.
         if row['Movement'] != measuredMovement:
-            finalMSG += f" {row['Team']} incorrect initiation line value.\n"
+            finalMSG += f"\n{row['Team']} incorrect initiation line value."
 
         # HANGING/PARK CHECK
+        # Convert park data
+        if row['Park'] == "Success":
+            parkStatus = 1
+        else:
+            parkStatus = 0
 
+        # Convert hang data
+        if row['Hang'] == "Success" or row['cargo'] == 1:
+            climbStatus = 1
+        else:
+            climbStatus = 0
+
+        # Check if you are climbing and parking at the same time
+        if climbStatus == 1 and parkStatus == 1:
+            finalMSG += f"\n{row['Team']} is both parking and climbing at the same time."
+
+        # Verify climbs are correct
+        measuredEndgame = allianceBreakdown[f'endgameRobot{drive_station}']
+        # Convert data
+        if measuredEndgame == "Hang":
+            measuredClimbStatus = 1
+            measuredParkStatus = 0
+        elif measuredEndgame == "Park":
+            measuredClimbStatus = 0
+            measuredParkStatus = 1
+        else:
+            measuredParkStatus = 0
+            measuredClimbStatus = 0
+        # Checking our data!
+        if climbStatus != measuredClimbStatus:
+            finalMSG += f"\n{row['Team']} climbing data is invalid, please review."
+        if parkStatus != measuredParkStatus:
+            finalMSG += f"\n{row['Team']} parking status is incorrect."
+
+        # Check scout names
+        if "null" in row['Scout']:
+            finalMSG += f"\n{row['Alliance']} has no scout name set."
+
+
+    # Check AUTO SCORES
+    # Inner scores
+    if redInnerAuto != scoreBreakdown['red']['autoCellsInner']:
+        finalMSG += f"\nRED alliance inner auto is off by {(redInnerAuto - scoreBreakdown['red']['autoCellsInner'])}"
+    if blueInnerAuto != scoreBreakdown['blue']['autoCellsInner']:
+        finalMSG += f"\nBLUE alliance inner auto is off by {(blueInnerAuto - scoreBreakdown['blue']['autoCellsInner'])}"
+
+    # Outer scores
+    if redOuterAuto != scoreBreakdown['red']['autoCellsOuter']:
+        finalMSG += f"\nRED alliance outer auto is off by {(redOuterAuto - scoreBreakdown['red']['autoCellsOuter'])}"
+    if blueOuterAuto != scoreBreakdown['blue']['autoCellsOuter']:
+        finalMSG += f"\nBLUE alliance outer auto is off by {(blueOuterAuto - scoreBreakdown['blue']['autoCellsOuter'])}"
+
+    # Lower scores
+    if redLowerAuto != scoreBreakdown['red']['autoCellsBottom']:
+        finalMSG += f"\nRED alliance lower auto is off by {(redLowerAuto - scoreBreakdown['red']['autoCellsBottom'])}"
+    if blueLowerAuto != scoreBreakdown['blue']['autoCellsBottom']:
+        finalMSG += f"\nBLUE alliance lower auto is off by {(blueLowerAuto - scoreBreakdown['blue']['autoCellsBottom'])}"
+
+    # Check TELE SCORES
+    # Inner scores
+    if redInnerTele != scoreBreakdown['red']['teleopCellsInner']:
+        finalMSG += f"\nRED alliance inner teleop is off by {(redInnerTele - scoreBreakdown['red']['teleopCellsInner'])}"
+    if blueInnerTele != scoreBreakdown['blue']['teleopCellsInner']:
+        finalMSG += f"\nBLUE alliance inner teleop is off by {(blueInnerTele - scoreBreakdown['blue']['teleopCellsInner'])}"
+
+    # Outer scores
+    if redOuterTele != scoreBreakdown['red']['teleopCellsOuter']:
+        finalMSG += f"\nRED alliance outer teleop is off by {(redOuterTele - scoreBreakdown['red']['teleopCellsOuter'])}"
+    if blueOuterTele != scoreBreakdown['blue']['teleopCellsOuter']:
+        finalMSG += f"\nBLUE alliance outer teleop is off by {(blueOuterTele - scoreBreakdown['blue']['teleopCellsOuter'])}"
+
+    # Lower scores
+    if redLowerTele != scoreBreakdown['red']['teleopCellsBottom']:
+        finalMSG += f"\nRED alliance lower teleop is off by {(redLowerTele - scoreBreakdown['red']['teleopCellsBottom'])}"
+    if blueLowerTele != scoreBreakdown['blue']['teleopCellsBottom']:
+        finalMSG += f"\nBLUE alliance lower teleop is off by {(blueLowerTele - scoreBreakdown['blue']['teleopCellsBottom'])}"
 
 
     if finalMSG == "":
-        finalMSG = "no changes needed, please close the frame"
+        finalMSG = "no changes needed, please close the window"
 
     return finalMSG
 
