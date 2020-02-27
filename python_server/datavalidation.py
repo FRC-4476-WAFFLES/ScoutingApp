0c6ff4c57e79cce360e0525b9036f90af7d87ba4
Brennan Bibic <brennan.bibic@gmail.com>
2020-02-27 17:18:47 -0500

    Bugfix for no internet error message

--- python_server/datavalidation.py
+++ python_server/datavalidation.py
@@ -53,7 +53,7 @@ def findErrors(df):
         matchNum = f"qm{mode(df['Match'])}"
         match_data = tba.match(f"{eventCode}_{matchNum}")
     except:
-        return " No consensus on match number!!! YO SCOUTS FIX YO STUFF"
+        return "\nNo consensus on match number/no internet connection."
 
 
     blueScore = match_data["alliances"]["blue"]["score"]
