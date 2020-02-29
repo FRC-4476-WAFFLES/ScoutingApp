36d4235ff9d63b8dbc48eff9ad9c704a386f21cd
Brennan Bibic <brennan.bibic@gmail.com>
2020-02-29 07:46:17 -0500

    Bugfix with sorting, added headers, fixed match 1 not being pulled

--- python_server/scheduleToCSV.py
+++ python_server/scheduleToCSV.py
@@ -1,12 +1,15 @@
 import csv
 import tbapy
+import operator
+
 
 apiKEY = "vMIHZYUhwQtwp5mB7hilezRBShGlfYTSmv8zPkcKxCHlTbmnYlQL7ikgf3YIDHmW"
 
 tba = tbapy.TBA(apiKEY)
-event_code = "2020misou"
+event_code = "2020onosh"
 
-schedule = []
+header = ["Qual", "R1", "R2", "R3", "B1", "B2", "B3"]
+schedule = list()
 matches_raw = tba.event_matches(event_code)
 for match in matches_raw:
     red = match["alliances"]['red']['team_keys']
@@ -22,5 +25,7 @@ for match in matches_raw:
 
     with open("Schedule.csv", 'w', newline='') as csvfile:
         wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        wr.writerows(schedule[1:len(schedule)])
+        schedule_sorted = sorted(schedule[0:len(schedule)], key=operator.itemgetter(0), reverse=False)
+        schedule_actual = [header] + schedule_sorted
+        wr.writerows(schedule_actual)
 
